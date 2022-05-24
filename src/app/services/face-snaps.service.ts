5a35dbff3821af0eede0a931f5796afcc8a0aa23
Camel Djoulako <cameldjoulako17@gmail.com>
2022-05-24 02:55:04 +0100

    services and http

--- src/app/services/face-snaps.service.ts
+++ src/app/services/face-snaps.service.ts
@@ -10,76 +10,7 @@ export class FaceSnapsService {
 
   constructor(private http: HttpClient) {}
 
-  faceSnaps: FaceSnap[] = [
-    {
-      id: 1,
-      title: 'Donald Z',
-      description: 'Mon meilleur ami depuis tout petit',
-      createdDate: new Date(),
-      snaps: 140,
-      imageUrl: './assets/images/developer.jpg',
-      location: 'Nkongsamba'
-    },
-    {
-      id: 2,
-      title: 'Camel D',
-      description: 'Mon meilleur ami depuis tout petit',
-      createdDate: new Date(),
-      snaps: 20,
-      imageUrl: './assets/images/developer.jpg',
-    },
-    {
-      id: 3,
-      title: 'Daniel S',
-      description: 'Mon meilleur ami depuis tout petit',
-      createdDate: new Date(),
-      snaps: 30,
-      imageUrl: './assets/images/developer.jpg'
-    },
-    {
-      id: 4,
-      title: 'Idriss M',
-      description: 'Mon meilleur partenaire depuis la fac',
-      createdDate: new Date(),
-      snaps: 0,
-      imageUrl: './assets/images/developer.jpg',
-      location: 'Douala'
-    },
-    {
-      id: 5,
-      title: 'Donald z',
-      description: 'Mon meilleur ami depuis tout petit',
-      createdDate: new Date(),
-      snaps: 10,
-      imageUrl: './assets/images/developer.jpg',
-      location: 'Nkongsamba'
-    },
-    {
-      id: 6,
-      title: 'Camel L',
-      description: 'Mon meilleur ami depuis tout petit',
-      createdDate: new Date(),
-      snaps: 0,
-      imageUrl: './assets/images/developer.jpg',
-    },
-    {
-      id: 7,
-      title: 'Daniel s',
-      description: 'Mon meilleur ami depuis tout petit',
-      createdDate: new Date(),
-      snaps: 30,
-      imageUrl: './assets/images/developer.jpg'
-    },
-    {
-      id: 8,
-      title: 'Idriss M',
-      description: 'Mon meilleur partenaire depuisla fac',
-      createdDate: new Date(),
-      snaps: 10,
-      imageUrl: './assets/images/developer.jpg',
-      location: 'Douala'
-    },
-  ];
+  faceSnaps: FaceSnap[] = [];
 
   /* getAllFaceSnaps(): FaceSnap[] {
     return this.faceSnaps;
@@ -89,22 +20,17 @@ export class FaceSnapsService {
     return this.http.get<FaceSnap[]>('http://localhost:3000/facesnaps');
   }
 
-  getSnapfaceSnapById(faceSnapId: number): FaceSnap {
-    const faceSnap = this.faceSnaps.find( faceSnap => faceSnap.id === faceSnapId);
-    if (!faceSnap) {
-      throw new Error('Facesnap not found');
-    } else {
-      return faceSnap;
+
+    getFaceSnapById(faceSnapId: number): Observable<FaceSnap> {
+      return this.http.get<FaceSnap>(`http://localhost:3000/facesnaps/${faceSnapId}`);
     }
 
-  }
+    snapfaceSnapById(faceSnapId: number, snapType: 'snap' | 'unsnap'): void {
+      //const faceSnap = this.getFaceSnapById(faceSnapId);
 
-  snapfaceSnapById(faceSnapId: number, snapType: 'snap' | 'unsnap'): void {
-    const faceSnap = this.getSnapfaceSnapById(faceSnapId);
+      //snapType === 'snap' ? faceSnap.snaps++ : faceSnap.snaps--;
 
-    snapType === 'snap' ? faceSnap.snaps++ : faceSnap.snaps--;
-
-  }
+    }
 
   addFaceSnap(formValue: { title: string, description: string, imageUrl: string, location?: string }) {
     const faceSnap: FaceSnap = {
--- src/app/single-face-snap/single-face-snap.component.ts
+++ src/app/single-face-snap/single-face-snap.component.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs';
 import { FaceSnapsService } from './../services/face-snaps.service';
 import { FaceSnap } from './../models/face-snap.models';
 import { Component, OnInit } from '@angular/core';
@@ -9,27 +10,29 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./single-face-snap.component.scss']
 })
 export class SingleFaceSnapComponent implements OnInit {
-  faceSnap!: FaceSnap;
+  faceSnap$!: Observable<FaceSnap>;
   buttonText!: string;
 
-  constructor(private faceSnapService: FaceSnapsService, private route: ActivatedRoute) {
+  constructor(private faceSnapsService: FaceSnapsService, private route: ActivatedRoute) {
 
    }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.buttonText = 'Oh Snap!';
     const faceSnapId = +this.route.snapshot.params['id'];
-    this.faceSnap = this.faceSnapService.getSnapfaceSnapById(faceSnapId);
+    this.faceSnap$ = this.faceSnapsService.getFaceSnapById(faceSnapId);
+
   }
 
   onSnap() {
-    if (this.buttonText === 'Oh Snap!') {
-      this.faceSnapService.snapfaceSnapById(this.faceSnap.id, 'snap');
+    /* if (this.buttonText === 'Oh Snap!') {
+      this.faceSnapsService.snapfaceSnapById(this.faceSnap.id, 'snap');
       this.buttonText = 'Oops UnSnap';
     } else {
-      this.faceSnapService.snapfaceSnapById(this.faceSnap.id, 'unsnap');
+
+      this.faceSnapsService.snapfaceSnapById(this.faceSnap.id, 'unsnap');
       this.buttonText = 'Oh Snap!';
-    }
+    } */
   }
 
 }
